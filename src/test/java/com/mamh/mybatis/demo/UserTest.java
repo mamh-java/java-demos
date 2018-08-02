6b8b492e88ebafec4afe7c2b7c76129ce4e2c668
mamh <bright.ma@blackshark.com>
2018-08-02 15:58:58 +0800

    add get all users test code
    Signed-off-by: mamh <bright.ma@blackshark.com>

--- src/test/java/com/mamh/mybatis/demo/UserTest.java
+++ src/test/java/com/mamh/mybatis/demo/UserTest.java
@@ -4,12 +4,53 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.Reader;
+import java.util.List;
 
 public class UserTest {
+    private String resource = "mybatis-config.xml";
+    private SqlSessionFactory sqlSessionFactory;
+    private SqlSession session;
+
+    @Before
+    public void before() {
+        InputStream is = getClass().getClassLoader().getResourceAsStream(resource);
+        sqlSessionFactory = new SqlSessionFactoryBuilder().build(is);
+        session = sqlSessionFactory.openSession();
+
+    }
+
+    @After
+    public void after() {
+        session.commit();
+        session.close();
+    }
+
+    @Test
+    public void testGetUser() {
+        String statement = "com.mamh.mybatis.demo.User" + ".getUser";
+        User user1 = session.selectOne(statement, 1);
+
+        System.out.println(user1);
+
+        User user2 = session.selectOne(statement, 2);
+
+        System.out.println(user2);
+    }
+
+    @Test
+    public void testGetAllUser() {
+        String statement = "com.mamh.mybatis.demo.User" + ".getAllUser";
+        List<User> user = session.selectList(statement);
+
+        System.out.println(user);
+    }
 
     @Test
     public void test() {
@@ -18,10 +59,17 @@ public class UserTest {
         try {
             reader = Resources.getResourceAsReader(resource);
             SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+
             SqlSession session = sqlSessionFactory.openSession();
-            User user = session.selectOne("getUser", 2);
+
+            String statement = "com.mamh.mybatis.demo.User" + ".getUser";
+            User user = session.selectOne(statement, 2);
+
+
             session.commit();
+
             System.out.println(user.getName());
+
         } catch (IOException e) {
             e.printStackTrace();
         }

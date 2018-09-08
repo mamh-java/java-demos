6003ad86c836ce93c68ad93a99afcd8e524b76ca
mamh <bright.ma@blackshark.com>
2018-09-08 13:21:01 +0800

    add different profiles demo
    Signed-off-by: mamh <bright.ma@blackshark.com>

--- src/main/java/com/mage/springboot/bean/Person.java
+++ src/main/java/com/mage/springboot/bean/Person.java
@@ -1,6 +1,5 @@
 package com.mage.springboot.bean;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
@@ -16,7 +15,6 @@ import java.util.Map;
 public class Person implements Serializable {
     private String lastName;
 
-    @Value("#{12*23}")
     private Integer age;
 
     private boolean isBoss;

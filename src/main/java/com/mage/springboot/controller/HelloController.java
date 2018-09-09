1b424d5c49113f68a4e65bf8010a1fef21e5b24a
mamh <bright.ma@blackshark.com>
2018-09-09 13:35:07 +0800

    add thymeleaf template demo code
    Signed-off-by: mamh <bright.ma@blackshark.com>

--- src/main/java/com/mage/springboot/controller/HelloController.java
+++ src/main/java/com/mage/springboot/controller/HelloController.java
@@ -1,9 +1,13 @@
 package com.mage.springboot.controller;
 
-import org.springframework.beans.factory.annotation.Value;
+import com.mage.springboot.bean.Student;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Arrays;
+import java.util.Map;
 
 @Controller
 public class HelloController {
@@ -20,5 +24,15 @@ public class HelloController {
     @ResponseBody
     public String say() {
         return "hello  " + name;
+    }
+
+    @RequestMapping("/success")
+    public String success(Map<String, Object> map) {
+        map.put("name", "<h1>你好你好</h1>");
+        map.put("user", Arrays.asList("张三", "李四", "王五"));
+        map.put("student", Arrays.asList(new Student("张三", 12),
+                new Student("王五", 34),
+                new Student("李四", 56)));
+        return "success";
     }
 }

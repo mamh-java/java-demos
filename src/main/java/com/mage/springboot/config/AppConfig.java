94f457e2d521d610d44f403e974ce4b09c48a325
mamh <bright.ma@blackshark.com>
2018-09-10 14:16:26 +0800

    add some entities java file and static html file
    Signed-off-by: mamh <bright.ma@blackshark.com>

--- src/main/java/com/mage/springboot/config/AppConfig.java
+++ src/main/java/com/mage/springboot/config/AppConfig.java
@@ -5,6 +5,7 @@ import com.mage.springboot.bean.Student;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
 @Configuration//指名这是一个配置类，用来添加组件的
 public class AppConfig {
     @Bean

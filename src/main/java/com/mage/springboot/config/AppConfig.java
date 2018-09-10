3462f942592828f9ca114b2ae6ba2784ab29435c
mamh <bright.ma@blackshark.com>
2018-09-10 15:18:27 +0800

    add localeResolver for different language
    Signed-off-by: mamh <bright.ma@blackshark.com>

--- src/main/java/com/mage/springboot/config/AppConfig.java
+++ src/main/java/com/mage/springboot/config/AppConfig.java
@@ -1,9 +1,13 @@
 package com.mage.springboot.config;
 
 import com.mage.springboot.bean.Student;
+import com.mage.springboot.resolver.LocalResolver;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @Configuration//指名这是一个配置类，用来添加组件的
@@ -11,5 +15,23 @@ public class AppConfig {
     @Bean
     public Student student() {
         return new Student("x", 321);
+    }
+
+    @Bean
+    public WebMvcConfigurer webMvcConfigurerAdapter() {
+        WebMvcConfigurer webMvcConfigurer = new WebMvcConfigurer() {
+            @Override
+            public void addViewControllers(ViewControllerRegistry registry) {
+                registry.addViewController("/").setViewName("index");
+                registry.addViewController("/index.html").setViewName("index");
+            }
+        };
+        return webMvcConfigurer;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        LocalResolver localResolver = new LocalResolver();
+        return localResolver;
     }
 }
---
+++ src/main/java/com/mage/springboot/resolver/LocalResolver.java
@@ -0,0 +1,41 @@
+package com.mage.springboot.resolver;
+
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.servlet.LocaleResolver;
+
+import java.util.Enumeration;
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class LocalResolver implements LocaleResolver {
+    public LocalResolver() {
+        System.err.println("==============================LocalResolver");
+    }
+
+
+    /**
+     * 点击链接切换语言，这里自定义了一个LocaleResolver。
+     *
+     * @param request
+     * @return
+     */
+    @Override
+    public Locale resolveLocale(HttpServletRequest request) {
+        String local = request.getParameter("l");
+        if (!StringUtils.isEmpty(local)) {
+            String[] split = local.split("_");
+            return new Locale(split[0], split[1]);
+        } else {
+            return request.getLocale();
+        }
+    }
+
+    @Override
+    public void setLocale(HttpServletRequest request, HttpServletResponse response, Locale locale) {
+
+    }
+}

85ddc3f9e0baaef14b93da4a298431bd93475d29
mamh <bright.ma@blackshark.com>
2018-09-11 14:13:40 +0800

    add edit employee
    Signed-off-by: mamh <bright.ma@blackshark.com>

--- src/main/java/com/mage/springboot/controller/EmployeeController.java
+++ src/main/java/com/mage/springboot/controller/EmployeeController.java
@@ -9,7 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import java.util.Collection;
 
@@ -37,10 +39,26 @@ public class EmployeeController {
         return "add";//添加员工页面
     }
 
+    @PutMapping("/emp")
+    public String edit(Employee employee) {
+        employeeDao.save(employee);
+        return "redirect:/emps";//员工列表页面
+    }
 
     @PostMapping("/emp")
     public String add(Employee employee) {//自动将请求参数封装为employee对象
         employeeDao.save(employee);
         return "redirect:/emps";//员工列表页面
+    }
+
+    @GetMapping("/emp/{id}")
+    public String toEditPage(@PathVariable("id") int id, Model model) {
+        Employee employee = employeeDao.get(id);
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("depts", departments);
+        model.addAttribute("emp", employee);
+
+        //回到修改页面
+        return "edit";
     }
 }

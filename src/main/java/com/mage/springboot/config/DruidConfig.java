15355e4b5a132d442e9d2423937b08dd467cc5a4
mamh <bright.ma@blackshark.com>
2018-10-19 15:51:55 +0800

    add druid monitor
    Signed-off-by: mamh <bright.ma@blackshark.com>

--- src/main/java/com/mage/springboot/config/DruidConfig.java
+++ src/main/java/com/mage/springboot/config/DruidConfig.java
@@ -1,11 +1,20 @@
 package com.mage.springboot.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.Filter;
 import javax.sql.DataSource;
 
 @Configuration
@@ -16,4 +25,29 @@ public class DruidConfig {
         return new DruidDataSource();
     }
 
+
+    @Bean
+    public ServletRegistrationBean servletRegistrationBean() {
+        ServletRegistrationBean bean = new ServletRegistrationBean<>(new StatViewServlet(), "/druid/*");
+        Map<String, String> initParameters = new HashMap<>();
+        initParameters.put("loginUsername", "admin");
+        initParameters.put("loginPassword", "123456");
+        initParameters.put("allow", "");
+
+        bean.setInitParameters(initParameters);
+        return bean;
+    }
+
+    @Bean
+    public FilterRegistrationBean filterRegistrationBean() {
+        FilterRegistrationBean<Filter> bean = new FilterRegistrationBean<>();
+        bean.setFilter(new WebStatFilter());
+        Map<String, String> initParameters = new HashMap<>();
+        initParameters.put("exclusions", "*.js,*.gif,*.jpg,*.bmp,*.png,*.css,*.ico,/druid/*");
+
+
+        bean.setInitParameters(initParameters);
+        bean.setUrlPatterns(Arrays.asList("/*"));
+        return bean;
+    }
 }
